af84976c8ca38b586b5befe63cdb5bef02b5a64d
Smootheez <131768200+Smootheez@users.noreply.github.com>
2024-11-07 19:14:55 +0700

    Clean up the code

--- src/main/java/dev/smootheez/scl/Scl.java
+++ src/main/java/dev/smootheez/scl/Scl.java
@@ -1,7 +1,5 @@
 package dev.smootheez.scl;
 
-import dev.smootheez.scl.registry.ConfigRegister;
-import dev.smootheez.scl.test.ExampleConfig;
 import net.fabricmc.api.ClientModInitializer;
 
 
@@ -9,6 +7,5 @@ public class Scl implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         Constants.LOGGER.info("Scl initialized");
-        ConfigRegister.getInstance().register(ExampleConfig.getInstance());
     }
 }
